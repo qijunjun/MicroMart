1eb3cfeddfe09aeface006cfc98ffb2cdd84b1ea
qijunjun <2269873327@qq.com>
2017-08-18 10:04:16 +0800

    [更新]页面标题

--- resources/js/views/orderList.js
+++ resources/js/views/orderList.js
@@ -12,10 +12,10 @@ $(function () {
     console.log(area);
     function getCity(province) {
         for (var i in area) {
-            if (area[i].id == province)
-                break;
+            if (area[i].id == province){
+                return {number: i, cities: area[i].cities};
+            }
         }
-        return {number: i, cities: area[i].cities};
     }
     //通过省份和城市获取地区
     function getDistrict(province, city) {
@@ -26,30 +26,54 @@ $(function () {
         }
         return area[getCity(province).number].cities[i].districts;
     }
-
-
     for(var i=0;i<area.length;i++){
         optionProvince += "<option value='"+area[i].id+"'>"+area[i].name+"</option>";
     }
     oProvince.append(optionProvince);
     // 省份改变，联动城市
+    // oProvince.change(function(){
+    //     console.log($(this).get(0).selectedIndex);
+    //     oCity.empty();
+    //     var optionCity = "<option value='0'>--请选择城市--</option>";
+    //     for(var j=0;j<getCity($(this).val()).cities.length;j++){
+    //         optionCity += "<option value='"+getCity($(this).val()).cities[j].id+"'>"+getCity($(this).val()).cities[j].name+"</option>";
+    //     }
+    //     oCity.append(optionCity);
+    // });
+    // 城市改变，联动区域
+    // oCity.change(function(){
+    //     oDistricts.empty();
+    //     var optionDistricts = "<option value='0'>--请选择地区--</option>";
+    //     for(var m=0;m<getDistrict($(this).prev().val(), $(this).val()).length;m++){
+    //         optionDistricts += "<option value='"+getDistrict($(this).prev().val(), $(this).val())[m].id+"'>"+getDistrict($(this).prev().val(), $(this).val())[m].name+"</option>";
+    //     }
+    //     oDistricts.append(optionDistricts);
+    // });
+
+    //省份改变，获取城市的方法二
     oProvince.change(function(){
+        //获取选中的索引值
+        console.log($(this).get(0).selectedIndex);
         oCity.empty();
         var optionCity = "<option value='0'>--请选择城市--</option>";
-        for(var j=0;j<getCity($(this).val()).cities.length;j++){
-            optionCity += "<option value='"+getCity($(this).val()).cities[j].id+"'>"+getCity($(this).val()).cities[j].name+"</option>";
+        oDistricts.empty();
+        for(var j=0;j<area[$(this).get(0).selectedIndex-1].cities.length;j++){
+            optionCity += "<option value='"+area[$(this).get(0).selectedIndex-1].cities[j].id+"'>"+area[$(this).get(0).selectedIndex-1].cities[j].name+"</option>";
         }
         oCity.append(optionCity);
     });
-    // 城市改变，联动区域
+    // 城市改变，联动区域方法二
     oCity.change(function(){
+        console.log(area[$(this).prev().get(0).selectedIndex-1].cities[$(this).get(0).selectedIndex-1].districts);
         oDistricts.empty();
         var optionDistricts = "<option value='0'>--请选择地区--</option>";
-        for(var m=0;m<getDistrict($(this).prev().val(), $(this).val()).length;m++){
-            optionDistricts += "<option value='"+getDistrict($(this).prev().val(), $(this).val())[m].id+"'>"+getDistrict($(this).prev().val(), $(this).val())[m].name+"</option>";
+        for(var m=0;m<area[$(this).prev().get(0).selectedIndex-1].cities[$(this).get(0).selectedIndex-1].districts.length;m++){
+            optionDistricts += "<option value='"+area[$(this).prev().get(0).selectedIndex-1].cities[$(this).get(0).selectedIndex-1].districts[m].id+"'>"+area[$(this).prev().get(0).selectedIndex-1].cities[$(this).get(0).selectedIndex-1].districts[m].name+"</option>";
         }
         oDistricts.append(optionDistricts);
     });
+
+
     $("ul li > div").click(function(){
         $("ul li input").removeClass("active");
         $(this).children("input").addClass("active");
